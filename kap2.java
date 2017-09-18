10124299d35ab2f9dabf551cdb668ff31f964757
Olof Rydh <ryol16@nti.umea>
2017-09-18 09:34:02 +0200

    .

---
+++ kap2.java
@@ -0,0 +1,18 @@
+/* Variabler & inputs
+   Olof Rydh
+   2017-09-18
+*/
+
+import javax.swing.*;
+import java.awt.*;
+
+public class kap2 {
+
+  public static void main(String[] args) {
+
+    String name = JOptionPane.showInputDialog("whats your name");
+    JOptionPane.showMessageDialog(null, "hey " + name);
+
+  }
+
+}
